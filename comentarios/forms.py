8044fbdbce5cd7a2b5ae5701ba9f3687c451b0bb
Jefferson Duarte <jeffersonsduarte@live.com>
2022-05-13 09:39:05 +0100

    Adicionado recaptcha no comentario

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,13 +1,34 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+        row_data = self.data
+        recaptcha_response = row_data.get('g-recaptcha-response')
+
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6Lenb-cfAAAAAN-bGZtawvkbqhdHFGhUYbxzZApd',
+                'response': recaptcha_response,
+            }
+
+        )
+
+        recaptcha_result = recaptcha_request.json()
+
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpa Mr. Robot, ocorreu um erro.'
+            )
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
         if len(nome) < 5:
             self.add_error(
